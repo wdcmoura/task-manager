b911f873047b7799b6497f7be5f7916dfa330fb3
William Moura <wdcmoura@gmail.com>
2026-06-14 11:38:40 -0300

    Alteracoes arquivos

---
+++ todo_project/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from todo_project import app, db
+
+@pytest.fixture(scope='session')
+def app():
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['WTF_CSRF_ENABLED'] = False
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
