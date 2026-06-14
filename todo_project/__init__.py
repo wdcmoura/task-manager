6a9d1a8b9461f421ef541160ac3abc7b4cc7e7cd
William Moura <wdcmoura@gmail.com>
2026-06-14 11:20:39 -0300

    Alteracao init

--- todo_project/__init__.py
+++ todo_project/__init__.py
@@ -8,18 +8,22 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'SQLALCHEMY_DATABASE_URI',
-    'sqlite:///site.db'
-)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'SQLALCHEMY_DATABASE_URI',
+        'sqlite:///site.db'
+    )
 
-db.init_app(app)
-bcrypt.init_app(app)
-login_manager.init_app(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
-login_manager.login_view = 'login'
+    login_manager.login_view = 'login'
 
-from . import routes
+    with app.app_context():
+        from . import routes
+
+    return app
