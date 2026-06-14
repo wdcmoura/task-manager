9b4bbcfed99f2b520154343e57ac7ed29ba3b0a6
William Moura <wdcmoura@gmail.com>
2026-06-14 11:16:44 -0300

    Alteracao Arquivo init

--- todo_project/__init__.py
+++ todo_project/__init__.py
@@ -1,7 +1,25 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
     'SQLALCHEMY_DATABASE_URI',
     'sqlite:///site.db'
 )
+
+db.init_app(app)
+bcrypt.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+from . import routes
